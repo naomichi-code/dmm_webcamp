c7211cf6958945e92b5c4f05831153c410f380c7
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-05-21 10:58:03 +0900

    [FInish]2-7

--- ruby/lesson6.rb
+++ ruby/lesson6.rb
@@ -1,5 +1,12 @@
-price =　200
+total_price = 100
 
-if price >= 100
+if total_price > 100
     puts 'みかんを購入、所持金に余りあり'
+
+elsif total_price == 100
+    puts 'みかんを購入、所持金は0円です'
+
+else
+    puts 'みかんを購入することができません'
+
 end
