473ccaf0a5027eb9f1f1a702a2623f40562a87d4
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-04 23:24:30 +0900

    [Finish]3-11

---
+++ rails/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '08dd8f872248b1257a9162dd8331c3567ed7a9cb6e8e0aef0f2d186e18f25aa8fba2a743d2358927c5ae01e58c30c2390e3b50ba23a8f59cfa5fdbce4d4cb962'
